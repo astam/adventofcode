b8820f069fd2071f795b7f3698df1f0d5b01897c
Arend Stam <arend.stam@gmail.com>
2023-12-20 11:44:06 +0100

    Improved day12-2

--- 2023/day12/code_part2-3.py
+++ 2023/day12/code_part2-3.py
@@ -1,12 +1,5 @@
-from multiprocessing import Pool, TimeoutError
-import time
-import os
-
-def f(x):
-    return x*x
-
+from multiprocessing import Pool
 import re
-from tqdm import tqdm
 
 def isvalid_pattern(pattern, ranges):
     split_pattern = [x for x in pattern.split('.') if x != '']
@@ -19,7 +12,7 @@ def isvalid_pattern(pattern, ranges):
 def get_valid_pattern_count(pattern, ranges, regex_pattern):
     # print()
     # print(pattern)
-    if re.match(regex_pattern, pattern):
+    if regex_pattern.match(pattern):
         if '?' not in pattern:
             # print(pattern)
             # print('found')
@@ -42,49 +35,17 @@ def process_line(line):
     # print()
     # print(pattern)
     # print(ranges)
-    regex_pattern = r'[\.\?]*' + r'[\.\?]+'.join([r'[#\?]{' + re.escape(x) + r'}' for x in ranges.split(',')])
+    regex_pattern = re.compile(r'[\.\?]*' + r'[\.\?]+'.join([r'[#\?]{' + re.escape(x) + r'}' for x in ranges.split(',')]))
     # print(regex_pattern)
     sub_sum = get_valid_pattern_count(pattern, ranges, regex_pattern)
     print(pattern + '->' + str(sub_sum))
     return sub_sum
 
 if __name__ == '__main__':
-    # start 4 worker processes
     # input_file = '2023/day12/input_example.txt'
     input_file = '2023/day12/input.txt'
     with open(input_file) as f:
         lines = [line.strip() for line in f.readlines()]
 
     with Pool(processes=8) as pool:
-
-        # print "[0, 1, 4,..., 81]"
-        print(pool.map(process_line, lines))
-
-        # # print same numbers in arbitrary order
-        # pool.imap_unordered(process_line, lines)
-        # for i in pool.imap_unordered(f, range(10)):
-        #     print(i)
-
-        # # evaluate "f(20)" asynchronously
-        # res = pool.apply_async(f, (20,))      # runs in *only* one process
-        # print(res.get(timeout=1))             # prints "400"
-
-        # # evaluate "os.getpid()" asynchronously
-        # res = pool.apply_async(os.getpid, ()) # runs in *only* one process
-        # print(res.get(timeout=1))             # prints the PID of that process
-
-        # # launching multiple evaluations asynchronously *may* use more processes
-        # multiple_results = [pool.apply_async(os.getpid, ()) for i in range(4)]
-        # print([res.get(timeout=1) for res in multiple_results])
-
-        # # make a single worker sleep for 10 seconds
-        # res = pool.apply_async(time.sleep, (10,))
-        # try:
-        #     print(res.get(timeout=1))
-        # except TimeoutError:
-        #     print("We lacked patience and got a multiprocessing.TimeoutError")
-
-        # print("For the moment, the pool remains available for more work")
-
-    # exiting the 'with'-block has stopped the pool
-    print("Now the pool is closed and no longer available")
+        print(sum(pool.map(process_line, lines)))
