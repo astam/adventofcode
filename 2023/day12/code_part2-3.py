592ff020c51dd059d44b461aacf513173a103ebc
Arend Stam <arend.stam@gmail.com>
2023-12-20 13:12:46 +0100

    Multiprocessing with taskbar

--- 2023/day12/code_part2-3.py
+++ 2023/day12/code_part2-3.py
@@ -1,5 +1,12 @@
 from multiprocessing import Pool
 import re
+# from tqdm import tqdm
+import time
+from tqdm.auto import tqdm
+from p_tqdm import p_map, p_umap, p_imap, p_uimap
+
+MAX_WORKERS = 5
+CHUNK_SIZE = 5
 
 def isvalid_pattern(pattern, ranges):
     split_pattern = [x for x in pattern.split('.') if x != '']
@@ -38,14 +45,42 @@ def process_line(line):
     regex_pattern = re.compile(r'[\.\?]*' + r'[\.\?]+'.join([r'[#\?]{' + re.escape(x) + r'}' for x in ranges.split(',')]))
     # print(regex_pattern)
     sub_sum = get_valid_pattern_count(pattern, ranges, regex_pattern)
-    print(pattern + '->' + str(sub_sum))
+    # print(pattern + '->' + str(sub_sum))
     return sub_sum
 
 if __name__ == '__main__':
-    # input_file = '2023/day12/input_example.txt'
+    input_file = '2023/day12/input_example.txt'
     input_file = '2023/day12/input.txt'
     with open(input_file) as f:
         lines = [line.strip() for line in f.readlines()]
+    # results = []
+    # for line in tqdm(lines):
+    #     result = process_line(line)
+    #     results.append(result)
 
-    with Pool(processes=8) as pool:
-        print(sum(pool.map(process_line, lines)))
+    results = p_umap(process_line, lines)
+
+    print(results)
+
+
+    # with Pool(processes=8) as pool:
+    #     print(sum(pool.map(process_line, lines)))
+
+    # with Pool(processes=8) as pool:
+    #     results = tqdm(
+    #         pool.map(
+    #             process_line,
+    #             lines, 
+    #             chunksize=CHUNK_SIZE
+    #         ),
+    #         total=len(lines),
+    #     )
+    #     print(results)
+
+    # pool = Pool(processes=8)
+
+    # result_list_tqdm = []
+    # for result in tqdm(pool.imap(func=process_line, iterable=lines), total=len(lines)):
+    #     result_list_tqdm.append(result)
+
+    # print(result_list_tqdm)
